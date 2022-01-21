422785e6413204f9f25532308c12150ad61db439
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-21 17:13:10 +0200

    fixed colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky") 
+            $('.navbar').addClass("sticky")
         }else{
             $('.navbar').removeClass("sticky")
         }
+        
     });
     // toggle menu navbar script
     $('.menuBtn').click(function(){
         $('.navbar .menu').toggleClass("active");
         let act = $('.menu').hasClass("active");
         if(act){
-            $('#menu-btn').css({'color':'white'})
+            $('#menu-btn').css({'color':'#FBFBFB'})
         }else{
-            $('#menu-btn').css({'color':'black'})
+            $('#menu-btn').css({'color':'#1B1B1B'})
         }
     });
 })
