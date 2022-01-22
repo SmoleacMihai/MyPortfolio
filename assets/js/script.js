c28c9354c9704145594981555c40990ba407c093
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-22 22:56:03 +0200

    finisshed scroll up buttono

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky")
+            $('.navbar').addClass("sticky");
         }else{
-            $('.navbar').removeClass("sticky")
+            $('.navbar').removeClass("sticky");
         }
-        
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
     });
 
     // toggle menu navbar script
